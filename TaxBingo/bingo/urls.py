6a5beeac9a69a7ae5db0f1534dd93b3964dc9db0
sowrd299 <ryan.ward46@gmail.com>
2020-03-09 15:16:00 -0700

    Implements logging out

--- TaxBingo/bingo/urls.py
+++ TaxBingo/bingo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'bingo'
 urlpatterns = [
-    path('do_login/', views.do_login),
+    path('do_login/', views.do_login, name='do_login'),
+    path('do_logout/', views.do_logout, name='do_logout'),
     path('', views.index, name='index') 
 ]
